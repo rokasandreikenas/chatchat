3ed7c6ebd130642f8ae37c3d31b54bf0d4dc578c
rokasandreikenas <shyptt@gmail.com>
2023-06-03 19:52:52 +0300

    remove commented code

--- src/components/__tests__/Input.test.tsx
+++ src/components/__tests__/Input.test.tsx
@@ -29,11 +29,5 @@ describe("Input", () => {
       "border",
       `1px solid ${theme.colors.lightGrey}`
     );
-    // expect(inputElement).toHaveStyle(`
-    //     padding: 0.75em;
-    //     width: 100%;
-    //     border-radius: 5px;
-    //     border: 1px solid ${theme.colors.lightGrey};
-    //   `);
   });
 });
