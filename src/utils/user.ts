5ff2fce942d4d048b4f095d3a21ea168819d655b
rokasandreikenas <shyptt@gmail.com>
2023-06-28 18:01:47 +0300

    Add getEmailInitials util function

--- src/utils/user.ts
+++ src/utils/user.ts
@@ -11,3 +11,17 @@ export const excludeUserEmail = (
 
   return participants.filter((participant) => participant !== user?.email);
 };
+
+export const getEmailInitials = (participants: Chat["participants"]) => {
+  if (!participants || participants.length === 0) {
+    return "";
+  }
+
+  const firstParticipant = participants[0];
+
+  if (!firstParticipant) {
+    return "";
+  }
+
+  return firstParticipant[0] || "";
+};
